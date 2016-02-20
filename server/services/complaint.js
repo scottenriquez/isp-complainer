82446ed683cc77bb5132969cf0a996b09103bc77
Scott Enriquez <scott.c.enriquez@gmail.com>
2016-02-19 23:14:55 -0600

    Fixing Atom formatting

--- server/services/complaint.js
+++ server/services/complaint.js
@@ -3,17 +3,17 @@ var Services = require('../services');
 var Promise = require('promise');
 
 module.exports = {
-  run: function() {
-    return new Promise(function(fulfill, reject) {
-      Services.speedtest.run().done(function(data) {
-        var promisedSpeed = Config.complaintConfig.promisedSpeed();
-        var actualSpeed = data.speeds.download;
-        if(actualSpeed < promisedSpeed) {
-          Services.twitter.complain(actualSpeed).done(function(response) {
-            fulfill(response);
-          });
+        run: function() {
+                return new Promise(function(fulfill, reject) {
+                        Services.speedtest.run().done(function(data) {
+                                var promisedSpeed = Config.complaintConfig.promisedSpeed();
+                                var actualSpeed = data.speeds.download;
+                                if(actualSpeed < promisedSpeed) {
+                                        Services.twitter.complain(actualSpeed).done(function(response) {
+                                                fulfill(response);
+                                        });
+                                }
+                        });
+                });
         }
-      });
-    });
-  }
 }

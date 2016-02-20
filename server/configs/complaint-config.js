ff67b7d990d39f34d0ad7312dc7dbddba54232bc
Scott Enriquez <scott.c.enriquez@gmail.com>
2016-02-19 23:11:20 -0600

    Fixing Atom formatting

--- server/configs/complaint-config.js
+++ server/configs/complaint-config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	tweetBody: function(promisedSpeed, actualSpeed, ispHandle) {
-    return ispHandle + ' I pay for ' + promisedSpeed + 'mbps down, but am getting ' + actualSpeed + 'mbps.';
-  },
-  ispHandle: function() {
-    return '@cableONE';
-  },
+	    return ispHandle + ' I pay for ' + promisedSpeed + 'mbps down, but am getting ' + actualSpeed + 'mbps.';
+	  },
+	  ispHandle: function() {
+	    return '@cableONE';
+	  },
 	promisedSpeed: function() {
 		return 100.0;
 	}

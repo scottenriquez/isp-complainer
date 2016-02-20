ea70dfb511a8e350b838e43464f7d817f4c31c72
Scott Enriquez <scott.c.enriquez@gmail.com>
2016-02-19 23:06:44 -0600

    Formatting fix

--- server/services/twitter.js
+++ server/services/twitter.js
@@ -5,16 +5,16 @@ var Config = require('../configs/');
 module.exports = {
 	complain: function(actualSpeed) {
 		return new Promise(function(fulfill, reject) {
-      var client = new Twitter({
-        consumer_key: Config.twitterAPIConfig.twitterConsumerKey(),
-        consumer_secret: Config.twitterAPIConfig.twitterConsumerSecret(),
-        access_token_key: Config.twitterAPIConfig.twitterAccessTokenKey(),
-        access_token_secret: Config.twitterAPIConfig.twitterAccessTokenSecret()
-      });
-      client.post('statuses/update', {status: Config.complaintConfig.tweetBody(Config.complaintConfig.promisedSpeed(), actualSpeed, Config.complaintConfig.ispHandle())},  function(error, tweet, response){
-        if(error) throw error;
-        fulfill(response);
-      });
+		      var client = new Twitter({
+		        consumer_key: Config.twitterAPIConfig.twitterConsumerKey(),
+		        consumer_secret: Config.twitterAPIConfig.twitterConsumerSecret(),
+		        access_token_key: Config.twitterAPIConfig.twitterAccessTokenKey(),
+		        access_token_secret: Config.twitterAPIConfig.twitterAccessTokenSecret()
+		      });
+		      client.post('statuses/update', {status: Config.complaintConfig.tweetBody(Config.complaintConfig.promisedSpeed(), actualSpeed, Config.complaintConfig.ispHandle())},  function(error, tweet, response){
+		        if(error) throw error;
+		        fulfill(response);
+		      });
 		});
 	}
 }

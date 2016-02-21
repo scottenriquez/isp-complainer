baf4760ba994ae6d218d0a1ae0edad57d43e0fdf
Scott Enriquez <scottie@enriquez.io>
2016-02-21 10:28:48 -0600

    Browser scheduling functionality implemented

--- public/app/app.js
+++ public/app/app.js
@@ -11,7 +11,8 @@ angular.module('ispComplainer').config(function($routeProvider, $locationProvide
         $routeProvider.otherwise({ templateUrl: '/partials/dashboard.jade', controller: 'DashboardController' });
 });
 
-angular.module('ispComplainer').controller('DashboardController', function($scope, $http, $compile, ComplaintService) {
+angular.module('ispComplainer').controller('DashboardController', function($scope, $http, $compile, $interval, ComplaintService) {
+        $scope.intervalLength = "15";
         $scope.manualTrigger = function() {
                 $scope.manualLoading = true;
                 ComplaintService.run().then(function(result) {
@@ -29,7 +30,29 @@ angular.module('ispComplainer').controller('DashboardController', function($scop
                 });
         };
         $scope.schedule = function() {
-                
+                $scope.scheduled = true;
+                $scope.scheduleStartDate = new Date().toLocaleDateString();
+                $scope.scheduleStartTime = new Date().toLocaleTimeString();
+                $scope.intervalCall = $interval(function() {
+                        $scope.scheduleLoading = true;
+                        ComplaintService.run().then(function(result) {
+                                var compiledHTML = $compile(
+                                        "<table class = 'table table-striped'><tr><th>Download Speed</th><th>Upload Speed</th><th>IP Address</th><th>Test Location</th><th>Distance</th><th>Ping</th><th>Twitter Post Status Code</th></tr>"
+                                        + "<tr><td>" + result.data.downloadSpeed + "</td>"
+                                        + "<td>" + result.data.uploadSpeed + "</td>"
+                                        + "<td>" + result.data.ipAddress + "</td>"
+                                        + "<td>" + result.data.testLocation + "</td>"
+                                        + "<td>" + result.data.distance + "</td>"
+                                        + "<td>" + result.data.ping + "</td>"
+                                        + "<td>" + result.data.twitterPostResponseCode + "</td></tr></table>")($scope);
+                                $("#schedule-results").append(compiledHTML);
+                                $scope.scheduleLoading = false;
+                        });
+                }, 60000 * parseInt($scope.intervalLength), 10000);
+        };
+        $scope.cancelSchedule = function() {
+                $interval.cancel($scope.intervalCall);
+                $scope.scheduled = false;
         };
 });
 
